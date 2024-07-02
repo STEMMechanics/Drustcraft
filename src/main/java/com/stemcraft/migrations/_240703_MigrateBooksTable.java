f0038b29fda5ad20ca6ddbeb8765038f30eacd20
James Collins <james.collins@outlook.com.au>
2024-07-03 08:34:34 +1000

    migrate books table to new format

---
+++ src/main/java/com/stemcraft/migrations/_240703_MigrateBooksTable.java
@@ -0,0 +1,45 @@
+package com.stemcraft.migrations;
+
+import com.stemcraft.SMBook;
+import com.stemcraft.SMDatabase;
+import com.stemcraft.SMDatabaseMigration;
+import com.stemcraft.STEMCraft;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Arrays;
+import java.util.List;
+
+public class _240703_MigrateBooksTable extends SMDatabaseMigration {
+
+    @Override
+    public void migrate() {
+        try {
+            PreparedStatement statement = SMDatabase.prepareStatement(
+                    "SELECT * FROM books");
+
+            assert statement != null;
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                SMBook book = SMBook.create(resultSet.getString("name"));
+                book.setAuthor(resultSet.getString("author"));
+                book.setTitle(resultSet.getString("title"));
+
+                List<String> contents = Arrays.stream(resultSet.getString("content").split("<n>")).toList();
+                book.setContent(contents);
+                book.save();
+            }
+
+            resultSet.close();
+            statement.close();
+
+            String sql = "DROP TABLE books";
+            statement = SMDatabase.prepareStatement("DROP TABLE books");
+            assert statement != null;
+            statement.executeQuery();
+            statement.close();
+        } catch (Exception e) {
+            STEMCraft.error(e);
+        }
+    }
+}
